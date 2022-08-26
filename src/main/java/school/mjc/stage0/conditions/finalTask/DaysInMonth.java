4a4dcc487aaf3c9b8ab90567bf2c83dabbf3ee83
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-08-26 20:31:13 +0600

    dghhn

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,14 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.time.LocalDate;
+
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year<0 || month<1 ||month>12){
+            System.out.println("invalid date");
+        }else {
+            System.out.println(LocalDate.of(year, month, 1).lengthOfMonth());
 
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        System.out.println(divider==0? "division by zero":dividend==divider*divider?"can be divided completely":"cannot be divided completely");
     }
 }
